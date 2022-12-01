87d4b543db8e2c397ed17ed10138b6524b798054
Progachewa <progachewa94@icloud.com>
2022-12-01 11:10:03 +0200

    Adding files

---
+++ script.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 23; i++) {
+  let h = i;
+  for (let e = 0; e <= 59; e++) {
+    let m = e;
+
+    console.log(`${h}:${m}`);
+  }
+}
+
+for (let num1 = 1; num1 <= 10; num1++) {
+  console.log(`${num1}*${num2}=${sum}`);
+  for (let num2 = 1; num2 <= 10; num2++) {
+    let sum = num1 * num2;
+  }
+}
