b3b3dd8e42ae9f04fa3b43840a30f3a24080b59c
Progachewa <progachewa94@icloud.com>
2022-12-04 19:01:51 +0200

    adding triangle js

---
+++ ConsoleTask/triangle.js
@@ -0,0 +1,15 @@
+// Triangle of Numbers:
+// Write a function, which receives a single number – n, and prints a triangle from 1 to n as in the examples.
+
+function triangle(number) {
+  debugger;
+  for (let i = 1; i <= number; i++) {
+    let result = "";
+    for (let e = 1; e <= i; e++) {
+      result += i;
+    }
+    console.log(result);
+  }
+}
+
+triangle(6);
