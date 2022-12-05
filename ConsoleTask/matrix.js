08909aec14a39fec4b1a935715ed3c6f11314ed0
krasimir.mladenov <krasimir.mladenov1@specsavers.com>
2022-12-05 14:49:14 +0200

    Added validation

--- ConsoleTask/matrix.js
+++ ConsoleTask/matrix.js
@@ -1,12 +1,19 @@
-function matrix(number) {
-  for (let m = 1; m <= number; m++) {
-    let result = "";
-    for (let n = 1; n <= number; n++) {
-      result += m + n - 1;
-      console.log();
+function matrix(input) {
+  let num = Number(input);
+
+  if (num && num < 20 && num >= 1) {    
+
+    for (let m = 1; m <= input; m++) {
+      let result = "";
+      for (let n = 1; n <= input; n++) {
+        result += m + n - 1;
+      }
+      console.log(result);
     }
-    console.log(result);
+
+  } else {
+    console.log('Please enter a number between 1 and 20!');
   }
 }
 
-matrix(3);
+matrix(4);
