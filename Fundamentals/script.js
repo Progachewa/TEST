c92b03f764d134f590282e7d77e9e9c314e184ab
Progachewa <progachewa94@icloud.com>
2022-12-05 21:19:27 +0200

    solution combination task

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,1 +1,23 @@
+// Напишете функция, която изчислява колко решения в естествените числа (включително и нулата) има уравнението:
+// x1 + x2 + x3 = n
+// Числото n е цяло число и се въвежда от конзолата.
+// Estimated time: 30min; Start: 21:00; End: 21:15;
 
+function combination(input) {
+  let num = Number(input);
+
+  if (num > 0) {
+    for (let i = 0; i <= num; i++) {
+      let result = "";
+      for (let e = 0; e <= num; e++) {
+        for (let a = 0; a <= num; a++) {
+          result = i + e + a;
+          if (result === num) {
+            console.log(result);
+          }
+        }
+      }
+    }
+  }
+}
+combination(["5"]);
