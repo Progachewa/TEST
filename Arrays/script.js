cb86bf23e89c1784fe217389108f28102e84a71c
Rogacheva <p.rogacheva@accenture.com>
2022-12-09 17:10:26 +0200

    adding concat();

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,1 +1,18 @@
+// concat();
+// Конкатенира два или повече масива. Връща нов масив.
 
+let a = ["1", "2", "3"];
+let b = ["4", "5", "6"];
+
+console.log(a.concat(b));
+
+let names = ["Pollie", "Lara"];
+let nums = [2, 3, 5];
+
+console.log(names.concat(nums));
+
+let c = ["1", "2", "3", "4", "5"];
+let d = ["6", "7", "8", "9", "10"];
+let e = ["11", "12", "13", "14", "15"];
+
+console.log(c.concat(d, e[0]));
