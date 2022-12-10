f31d7529456a7f47d441d04e4484301de9d3186c
Rogacheva <p.rogacheva@accenture.com>
2022-12-10 15:06:50 +0200

    adding fill();

--- Arrays/script.js
+++ Arrays/script.js
@@ -22,26 +22,44 @@
 // every();
 //Обикаля всички елементи от масива и връща "true" или "false". Ако всеки един елемент отговаря на определеното условие( на функцията ) връща "true", но ако само един елемент се окаже "false" - връща цялото "false".
 
-let a = ["1", "2", "3"];
+// let a = ["1", "2", "3"];
 
-function isNum(b) {
-  return typeof b === "number";
-}
+// function isNum(b) {
+//   return typeof b === "number";
+// }
 
-console.log(a.every(isNum)); // false
+// console.log(a.every(isNum)); // false
 
-let c = [1, 3, 5, 7];
+// let c = [1, 3, 5, 7];
 
-function isOdd(d) {
-  return d % 2 !== 0;
-}
+// function isOdd(d) {
+//   return d % 2 !== 0;
+// }
 
-console.log(c.every(isOdd)); // true
+// console.log(c.every(isOdd)); // true
 
-let arr = [10, 50, 100];
+// let arr = [10, 50, 100];
 
-function isBigger(element) {
-  return element >= 10;
-}
+// function isBigger(element) {
+//   return element >= 10;
+// }
 
-console.log(arr.every(isBigger)); // true
+// console.log(arr.every(isBigger)); // true
+
+//-----------------------------------------------------------
+//fill();
+// Пренаписва елемент/и от масива с определена стойност. Може да се подаде стартова позиция и финална.Ако не се подадат се пренаписват всички елементи с подадената стойност.
+
+let fruits = ["Banana", "Apple", "Strawberry"];
+
+console.log(fruits.fill("kiwi"));
+
+let vegetables = ["Cucumber", "Pepper", "Onion"];
+
+console.log(vegetables.fill("Carrot", 2, 3)); // "Onion" = "Carrot";
+
+let stars = Array(5).fill({});
+stars[0].name = "somename";
+stars[1].ages = 25;
+
+console.log(stars);
